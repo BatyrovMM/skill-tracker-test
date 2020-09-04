9a989df52177abeb550e3519f2c6e17b2f0901ff
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-09-04 15:58:56 +0300

    Updates

---
+++ precache-manifest.abf38d22a968178bc1e3536ecd6892a8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ad9f1c8a556fa0b1db512599fc0e9d",
+    "url": "/skill-tracker-test/index.html"
+  },
+  {
+    "revision": "bd434aed3cd19fef1d13",
+    "url": "/skill-tracker-test/static/css/main.b407b8c1.chunk.css"
+  },
+  {
+    "revision": "fc5083c7adeb9e5ecf5c",
+    "url": "/skill-tracker-test/static/js/2.4ac1154c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skill-tracker-test/static/js/2.4ac1154c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd434aed3cd19fef1d13",
+    "url": "/skill-tracker-test/static/js/main.15c31b8a.chunk.js"
+  },
+  {
+    "revision": "056d69bdb41ee6d66f91",
+    "url": "/skill-tracker-test/static/js/runtime-main.528b1d90.js"
+  },
+  {
+    "revision": "4754141bffc0369eed6b8106463bcf4c",
+    "url": "/skill-tracker-test/static/media/bell-icon.4754141b.svg"
+  },
+  {
+    "revision": "e85b5438f76cecdcec1b1d864e3fae8e",
+    "url": "/skill-tracker-test/static/media/chat-icon.e85b5438.svg"
+  },
+  {
+    "revision": "fea86d2352fbc00bb3bd256b2d4765d3",
+    "url": "/skill-tracker-test/static/media/facebook-logo.fea86d23.svg"
+  },
+  {
+    "revision": "d3d9efd65a68b4d4af0858f9f9dbd5ec",
+    "url": "/skill-tracker-test/static/media/info-icon.d3d9efd6.svg"
+  },
+  {
+    "revision": "9c40e6387fa53a29439b2f95206ee8ad",
+    "url": "/skill-tracker-test/static/media/inst-logo.9c40e638.svg"
+  },
+  {
+    "revision": "49526048d86405dda8b71bc154b3cd79",
+    "url": "/skill-tracker-test/static/media/kid-avatar.49526048.png"
+  },
+  {
+    "revision": "4e2338c8fbe929eb64db8a4e30c0fdd0",
+    "url": "/skill-tracker-test/static/media/lock-icon.4e2338c8.svg"
+  },
+  {
+    "revision": "8a194ce18e12d86ca1451389bb370a58",
+    "url": "/skill-tracker-test/static/media/mobile-menu.8a194ce1.svg"
+  },
+  {
+    "revision": "8b86d82d6d07ed26f7b3f5af45b71b9d",
+    "url": "/skill-tracker-test/static/media/pencil-icon.8b86d82d.svg"
+  },
+  {
+    "revision": "b19576c356916bca7332a9afd4ac887d",
+    "url": "/skill-tracker-test/static/media/search-icon.b19576c3.svg"
+  },
+  {
+    "revision": "fc42626b3a11ec0c53c5fddaa5c778c6",
+    "url": "/skill-tracker-test/static/media/skillTracker-icon.fc42626b.svg"
+  },
+  {
+    "revision": "6e0d568de63976b46d7f6590c95c2bd7",
+    "url": "/skill-tracker-test/static/media/star-icon.6e0d568d.svg"
+  },
+  {
+    "revision": "4802bb582cf08763302f0cc8115925d5",
+    "url": "/skill-tracker-test/static/media/vk-logo.4802bb58.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b74afff27c3c52262906e84d31219d85.js"
+  "/skill-tracker-test/precache-manifest.abf38d22a968178bc1e3536ecd6892a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/skill-tracker-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
