1a9188885484a46a626511e606ddf6b62c057dc4
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-09-04 15:50:06 +0300

    Updates

---
+++ precache-manifest.c2df0c37118ce5f0822ae0f02b5d0626.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd748b410c20f1ebd130cf40b0da25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adfc971fc33c84e2e9f1",
+    "url": "/static/css/main.33555251.chunk.css"
+  },
+  {
+    "revision": "fc5083c7adeb9e5ecf5c",
+    "url": "/static/js/2.4ac1154c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ac1154c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adfc971fc33c84e2e9f1",
+    "url": "/static/js/main.921a4a06.chunk.js"
+  },
+  {
+    "revision": "5b7899560a67d7c662ff",
+    "url": "/static/js/runtime-main.1e1f90b1.js"
+  },
+  {
+    "revision": "4754141bffc0369eed6b8106463bcf4c",
+    "url": "/static/media/bell-icon.4754141b.svg"
+  },
+  {
+    "revision": "e85b5438f76cecdcec1b1d864e3fae8e",
+    "url": "/static/media/chat-icon.e85b5438.svg"
+  },
+  {
+    "revision": "fea86d2352fbc00bb3bd256b2d4765d3",
+    "url": "/static/media/facebook-logo.fea86d23.svg"
+  },
+  {
+    "revision": "d3d9efd65a68b4d4af0858f9f9dbd5ec",
+    "url": "/static/media/info-icon.d3d9efd6.svg"
+  },
+  {
+    "revision": "9c40e6387fa53a29439b2f95206ee8ad",
+    "url": "/static/media/inst-logo.9c40e638.svg"
+  },
+  {
+    "revision": "49526048d86405dda8b71bc154b3cd79",
+    "url": "/static/media/kid-avatar.49526048.png"
+  },
+  {
+    "revision": "4e2338c8fbe929eb64db8a4e30c0fdd0",
+    "url": "/static/media/lock-icon.4e2338c8.svg"
+  },
+  {
+    "revision": "8a194ce18e12d86ca1451389bb370a58",
+    "url": "/static/media/mobile-menu.8a194ce1.svg"
+  },
+  {
+    "revision": "8b86d82d6d07ed26f7b3f5af45b71b9d",
+    "url": "/static/media/pencil-icon.8b86d82d.svg"
+  },
+  {
+    "revision": "b19576c356916bca7332a9afd4ac887d",
+    "url": "/static/media/search-icon.b19576c3.svg"
+  },
+  {
+    "revision": "fc42626b3a11ec0c53c5fddaa5c778c6",
+    "url": "/static/media/skillTracker-icon.fc42626b.svg"
+  },
+  {
+    "revision": "6e0d568de63976b46d7f6590c95c2bd7",
+    "url": "/static/media/star-icon.6e0d568d.svg"
+  },
+  {
+    "revision": "4802bb582cf08763302f0cc8115925d5",
+    "url": "/static/media/vk-logo.4802bb58.svg"
+  }
+]);
